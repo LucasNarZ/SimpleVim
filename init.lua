135ee2bb79e6c8bdbfb8bef5a7944e27f53874ed
LucasNarZ <LucasNarloch123@gmail.com>
2025-07-30 19:45:11 -0300

    make all yanks go to clipboard

--- init.lua
+++ init.lua
@@ -6,4 +6,18 @@ local nvim_lsp = require("lspconfig")
 nvim_lsp.tsserver.setup{}
 vim.opt.relativenumber = true
 vim.opt.number = true
-vim.opt.clipboard = "unnamedplus"
+local opts = { noremap = true, silent = true }
+
+-- Normal mode
+vim.keymap.set("n", "y", '"+y', opts)
+vim.keymap.set("n", "yy", '"+yy', opts)
+vim.keymap.set("n", "p", '"+p', opts)
+vim.keymap.set("n", "P", '"+P', opts)
+vim.keymap.set("n", "d", '"+d', opts)
+vim.keymap.set("n", "dd", '"+dd', opts)
+
+-- Visual mode
+vim.keymap.set("v", "y", '"+y', opts)
+vim.keymap.set("v", "d", '"+d', opts)
+vim.keymap.set("v", "p", '"+p', opts)
+vim.keymap.set("v", "P", '"+P', opts)
