5ece98fd4659e7bb8c485a900d442dcfd20da21b
LucasNarZ <LucasNarloch123@gmail.com>
2025-08-01 10:44:25 -0300

    restore tree plugin config

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,18 +1,10 @@
 return {
-    "kyazdani42/nvim-tree.lua",
-    config = function()
-	    require("nvim-tree").setup({
-		    filters = {
-			    git_ignored = false
-		    },
-		    view = {
-			    mapping = {
-				    custom_only = false,
-				    list = {
-					  { key = "<Space>", action = "edit_or_open", mode = "n" }
-				    }
-			    }
-		    }
-	    })
-    end,
+	"kyazdani42/nvim-tree.lua",
+	config = function()
+		require("nvim-tree").setup({
+			filters = {
+				git_ignored = false
+			},
+		})
+	end,
 }
