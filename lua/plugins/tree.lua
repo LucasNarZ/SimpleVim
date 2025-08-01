c6e2b9b0d76ddb4600e470156e827803122728da
LucasNarZ <LucasNarloch123@gmail.com>
2025-08-01 20:34:44 -0300

    open folder and files using space keymap

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -5,6 +5,15 @@ return {
 			filters = {
 				git_ignored = false
 			},
+			on_attach = function(bufnr)
+				local api = require("nvim-tree.api")
+				local function opts(desc)
+					return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+				end
+
+				vim.keymap.set("n", "<Space>", api.node.open.edit, opts("Open"))
+				vim.keymap.set("n", "<Space>", api.node.open.edit, opts("Open")) 
+			end
 		})
 	end,
 }
