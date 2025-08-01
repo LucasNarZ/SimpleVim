51c3800971c6d53962d4f8443d1e6bf2bac777bc
LucasNarZ <LucasNarloch123@gmail.com>
2025-08-01 10:45:06 -0300

    improve keymaps organization

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,4 +4,18 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
-vim.keymap.set("t", "<h-n>", "<C-\\><h-c><C-\\>", { noremap = true, silent = true })
+local opts = { noremap = true, silent = true }
+
+-- Normal mode
+vim.keymap.set("n", "y", '"+y', opts)
+vim.keymap.set("n", "yy", '"+yy', opts)
+vim.keymap.set("n", "p", '"+p', opts)
+vim.keymap.set("n", "P", '"+P', opts)
+vim.keymap.set("n", "d", '"+d', opts)
+vim.keymap.set("n", "dd", '"+dd', opts)
+
+-- Visual mode
+vim.keymap.set("v", "y", '"+y', opts)
+vim.keymap.set("v", "d", '"+d', opts)
+vim.keymap.set("v", "p", '"+p', opts)
+vim.keymap.set("v", "P", '"+P', opts)
