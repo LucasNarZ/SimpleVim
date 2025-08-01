c37e138a5a8b5e19e4363ee9e30b42f13c4807b5
LucasNarZ <LucasNarloch123@gmail.com>
2025-08-01 19:55:47 -0300

    show errors in the line

--- init.lua
+++ init.lua
@@ -1,6 +1,16 @@
 require("config.lazy")
 require("config.keymaps")
 
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = '●',
+    spacing = 4,
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
 
 vim.opt.relativenumber = true
 vim.opt.number = true
