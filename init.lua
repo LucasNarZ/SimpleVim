103d856e561901306128a98be6fbc7958e9a0ba0
LucasNarZ <LucasNarloch123@gmail.com>
2025-08-02 18:37:14 -0300

    fix indention

--- init.lua
+++ init.lua
@@ -15,5 +15,23 @@ vim.diagnostic.config({
 vim.opt.relativenumber = true
 vim.opt.number = true
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "typescript", "typescriptreact", "javascriptreact", "html", "css", "lua", "json", "yaml", "markdown" },
+  callback = function()
+    vim.bo.tabstop = 2
+    vim.bo.shiftwidth = 2
+    vim.bo.expandtab = true
+  end
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {"c", "lua", "c++"},
+  callback = function()
+    vim.bo.tabstop = 4
+    vim.bo.shiftwidth = 4
+    vim.bo.expandtab = true
+  end
+})
+
 vim.lsp.buf.code_action()
 
