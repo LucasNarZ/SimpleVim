75d38b21eb4c8b0ea5f30d758f2df1f759ac65ce
LucasNarZ <LucasNarloch123@gmail.com>
2025-09-24 07:29:02 -0300

    change keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,20 +7,6 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help ta
 
 local opts = { noremap = true, silent = true }
 
--- Normal mode
-vim.keymap.set("n", "y", '"+y', opts)
-vim.keymap.set("n", "yy", '"+yy', opts)
-vim.keymap.set("n", "p", '"+p', opts)
-vim.keymap.set("n", "P", '"+P', opts)
-vim.keymap.set("n", "d", '"+d', opts)
-vim.keymap.set("n", "dd", '"+dd', opts)
-
--- Visual mode
-vim.keymap.set("v", "y", '"+y', opts)
-vim.keymap.set("v", "d", '"+d', opts)
-vim.keymap.set("v", "p", '"+p', opts)
-vim.keymap.set("v", "P", '"+P', opts)
-
 -- ToggleTerm
 vim.keymap.set('t', '<C-x>', '<C-\\><C-n><Cmd>ToggleTerm<CR>', opts) 
 
