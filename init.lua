0e25a39a4cab098eb48fdd966b16a2c55ae0c7d1
LucasNarZ <lucasnarloch123@gmail.com>
2026-01-28 07:28:38 -0300

    refactor: remove folding

--- init.lua
+++ init.lua
@@ -1,31 +1,8 @@
 require("config.lazy")
 require("config.keymaps")
 
-vim.diagnostic.config({
-    virtual_text = {
-        prefix = '●',
-        spacing = 4,
-    },
-    signs = true,
-    underline = true,
-    update_in_insert = false,
-    severity_sort = true,
-})
-
 vim.opt.relativenumber = true
 vim.opt.number = true
-
-vim.o.foldenable = true
-vim.o.foldmethod = "indent"
-vim.o.foldlevel = 99
-vim.opt.fillchars = { eob = " ", fold = " " }
-vim.opt.foldtext = "v:lua.custom_fold_text()"
-
-function _G.custom_fold_text()
-    local line = vim.fn.getline(vim.v.foldstart)
-    local lines_count = vim.v.foldend - vim.v.foldstart + 1
-    return "▶ " .. line .. " ... (" .. lines_count .. " lines)"
-end
 
 vim.api.nvim_create_autocmd("FileType", {
     pattern = { "javascript", "typescript", "typescriptreact", "javascriptreact", "html", "css", "lua", "json", "markdown", "c", "c++", "lua", "header" },
@@ -43,6 +20,18 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.bo.shiftwidth = 2
         vim.bo.expandtab = true
     end
+})
+
+vim.diagnostic.config({
+    virtual_text = false,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+    float = {
+        border = "rounded",
+        source = "always",
+    },
 })
 
 vim.o.wrap = true
